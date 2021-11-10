02cd5452f7b5f10f42ec3805dcf3ad18777e51ac
Andersson Johan Ramirez Mosquera <46058572+SirAnderss@users.noreply.github.com>
2021-11-10 17:20:03 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './css/index.css';
-import { makeServer } from "./server"
+import { makeServer } from './serve';
 
 if (process.env.NODE_ENV === "development") {
   makeServer({ environment: "development" })
